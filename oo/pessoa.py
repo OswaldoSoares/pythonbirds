c0b0155e2be8d8c4c432a31d065c8023e978038e
Oswaldo <oswaldosoares@gmail.com>
2020-10-17 22:04:09 -0300

    Criados atributos complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá{id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Cleusa')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Oswaldo'
-    print(p.nome)
-    print(p.idade)
+    cleusa = Pessoa(nome='Cleusa')
+    Oswaldo = Pessoa(cleusa, nome='Oswaldo')
+    print(Pessoa.cumprimentar(Oswaldo))
+    print(id(Oswaldo))
+    print(Oswaldo.cumprimentar())
+    print(Oswaldo.nome)
+    print(Oswaldo.idade)
+    for filho in Oswaldo.filhos:
+        print(filho.nome)
