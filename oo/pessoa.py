e34c347cc99261a0ce03e59c53f0709574bfca16
OswaldoSoares <oswaldosoares@gmail.com>
2020-12-10 13:38:27 -0300

    Testes Fase finalizado, conforme aula

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá{id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -18,16 +18,37 @@ class Pessoa:
         return f'{cls} - olhos{cls.olhos}'
 
 
+class Mutante(Pessoa):
+    olhos = 5
+
+
+class Homem(Pessoa):
+    pass
+
+    def cumprimentar(self):
+        cumprimentar_antes = super().cumprimentar()
+        return f'{cumprimentar_antes}. Aperto de mão'
+
+
+
+
+
 if __name__ == '__main__':
-    cleusa = Pessoa(nome='Cleusa')
-    Oswaldo = Pessoa(cleusa, nome='Oswaldo')
+    cleusa = Mutante(nome='Cleusa')
+    Oswaldo = Homem(cleusa, nome='Oswaldo')
     print(Pessoa.cumprimentar(Oswaldo))
     print(id(Oswaldo))
     print(Oswaldo.cumprimentar())
     print(Oswaldo.nome)
     print(Oswaldo.idade)
+    print(Oswaldo.__dict__)
     for filho in Oswaldo.filhos:
         print(filho.nome)
     print(Pessoa.metodo_estatico(), Oswaldo.metodo_estatico())
-    print(Pessoa.nome_e1_atributos(), Oswaldo.nome_e_atributos())
+    print(Pessoa.nome_e_atributos(), Oswaldo.nome_e_atributos())
+    print(Oswaldo.olhos)
+    print(cleusa.olhos)
+    print(Oswaldo.cumprimentar())
+    print(cleusa.cumprimentar())
+
 
